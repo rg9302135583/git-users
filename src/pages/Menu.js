e3ceda58a2abe2b08c418a9f3d552f9f52c139db
Aakash Prajapat <aakashprajapat2020@gmail.com>
2022-02-12 22:38:37 +0530

    material static table created.

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,26 +1,30 @@
 import React from "react";
-import { MenuList } from "../helpers/MenuList";
+// import { MenuList } from "../helpers/MenuList";
 import MenuItem from "../components/MenuItem";
 import "../styles/Menu.css";
 
 
 function Menu() {
+
   return (
-    <div className="menu">
-      <h1 className="menuTitle">User Public Repo List</h1>
-      <div className="menuList">
-        {MenuList.map((menuItem, key) => {
-          console.log("menuItem",menuItem)
-          return (
-            <MenuItem
+    <div>
+
+    {/* // <div className="menu"> */}
+        {/* <div className="menuList"> */}
+        {/* {MenuList.map((menuItem, key) => { */}
+        {/* console.log("menuItem",menuItem) */}
+        {/* return ( */}
+        {/* <MenuItem
               key={key}
               image={menuItem.image}
               name={menuItem.name}
               price={menuItem.price}
-            />
-          );
-        })}
-      </div>
+            /> */}
+        {/* ); */}
+        {/* })} */}
+        <MenuItem />
+        {/* </div> */}
+    {/* // </div> */}
     </div>
   );
 }
