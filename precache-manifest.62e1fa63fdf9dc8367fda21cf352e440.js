e7a4e2425cf28fbe40599ac9b939cc5737ba5064
unknown <rg9302135583@gmail.com>
2022-02-15 22:54:41 +0530

    Updates

---
+++ precache-manifest.62e1fa63fdf9dc8367fda21cf352e440.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9816d592cb8357f7bd7e6250d2f18d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a3428514753e566a2e1",
+    "url": "/static/css/main.d2d1378e.chunk.css"
+  },
+  {
+    "revision": "9adf44f1b7f87ed3d653",
+    "url": "/static/js/2.202485d0.chunk.js"
+  },
+  {
+    "revision": "c36dbbca9f49715d3f32",
+    "url": "/static/js/3.7c81545f.chunk.js"
+  },
+  {
+    "revision": "7a3428514753e566a2e1",
+    "url": "/static/js/main.bc3dbfe5.chunk.js"
+  },
+  {
+    "revision": "a44f61c1cb83e46dd205",
+    "url": "/static/js/runtime~main.112d2f2d.js"
+  },
+  {
+    "revision": "b1aa3ca957dc54f0d8527780c0adfcf9",
+    "url": "/static/media/CDN-Logo.b1aa3ca9.png"
+  },
+  {
+    "revision": "605152f0edd5d37a283a91fe5806b28c",
+    "url": "/static/media/IMG_20220215_084111.605152f0.jpg"
+  },
+  {
+    "revision": "59f69e0395d94744de7649786b2f8f20",
+    "url": "/static/media/IMG_20220215_084139.59f69e03.jpg"
+  },
+  {
+    "revision": "ebd31d4ac9d1631036cb6a455598ef3a",
+    "url": "/static/media/hexagons-of-different-sizes-on-a-blue-background-vector.ebd31d4a.jpg"
+  }
+]);
